cd338537147d2210497c6fc0ec8db7da201e9420
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 15:45:34 +0200

    implemented new game frame

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-game',
   templateUrl: './new-game.component.html',
-  styleUrls: ['./new-game.component.css']
+  styleUrls: ['./new-game.component.css'],
 })
 export class NewGameComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  public ngOnInit(): void {}
 }
