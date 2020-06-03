b37896e0e84662de7e8ad97edb00fc2166e95fde
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 18:48:47 +0200

    getting user from local-stroaghe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+
+import { SetUser } from './store/leaderboard/leaderboard.actions';
+
 @Component({
   selector: 'app-root',
   template: `
     <app-header></app-header>
-    <router-outlet><router-outlet>
+    <router-outlet></router-outlet>
   `,
-  styles: ['']
+  styles: [''],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private store: Store<{}>) {
+    this.checkForUser();
+  }
+
+  private checkForUser(): void {
+    const user = window.localStorage.getItem('user');
+    return user ? this.store.dispatch(new SetUser(user)) : null;
+  }
+}
