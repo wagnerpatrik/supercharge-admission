71ca8229a1edc212add2519ac15577cbfec7ae8c
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 14:57:22 +0200

    added card constants

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,14 @@
+export const ASSETS_PATH = './assets/cards/';
+export const EXTENSION = '.png';
+export const TECHNOLOGIES = [
+  'angular',
+  'd3',
+  'jenkins',
+  'postcss',
+  'react',
+  'redux',
+  'sass',
+  'supercharge',
+  'ts',
+  'webpack',
+];
