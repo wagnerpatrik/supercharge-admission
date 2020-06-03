19abd0b1c51464034c2f7555e79cf1d3616f202c
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 14:33:19 +0200

    Added store to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+const REDUCERS = {};
+const EFFECTS = [];
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(REDUCERS),
+    EffectsModule.forRoot(EFFECTS),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
