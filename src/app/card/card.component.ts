302a3e838429ed96e63f8be030838fd8522e1037
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 15:34:04 +0200

    implemented card style and basic flip

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,13 +5,16 @@ import { Card } from '../shared/models';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
   @Input() public card: Card;
   public isCardFlipped = false;
 
-  constructor() { }
+  constructor() {}
   public ngOnInit(): void {}
-  public flipCard({ id }: Card) {}
+
+  public flipCard() {
+    this.isCardFlipped = !this.isCardFlipped;
+  }
 }
