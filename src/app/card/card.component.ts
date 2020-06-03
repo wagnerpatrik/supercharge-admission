8ffbac06f46587cc838a238c132ee39179a7fe4d
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 15:18:25 +0200

    implemented card without style

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Card } from '../shared/models';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() public card: Card;
+  public isCardFlipped = false;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  public ngOnInit(): void {}
+  public flipCard({ id }: Card) {}
 }
