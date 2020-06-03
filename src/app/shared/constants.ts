a47b0644c24bbaa00b47a1a2964aa0098b040745
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 18:14:54 +0200

    added db url, and s-key

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -14,3 +14,6 @@ export const TECHNOLOGIES = [
 ];
 
 export const DEFAULT_DECK_SIZE = 6;
+
+export const DB_URL = 'https://api.jsonbin.io/b/5ed7cc1379382f568bd27194';
+export const SECRET_KEY = '$2b$10$pvpwUUz/M1DN/is2uIMbQ.xD2xpN7ThMF7/SnBhFDIrhZ3PEOMooK';
