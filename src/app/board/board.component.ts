70f1484b380891439b941671a47517ab32d88dd2
Patrik Wagner <wagnerp@starschema.net>
2020-06-03 15:33:16 +0200

    displaying cards on board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { Card } from '../shared/models';
+import { BoardService } from '../shared/board.service';
+import { DEFAULT_DECK_SIZE } from '../shared/constants';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
+  public deck$: Observable<Card[]>;
 
-  constructor() { }
+  constructor(private boardService: BoardService) {}
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.deck$ = this.boardService.getDeck(DEFAULT_DECK_SIZE);
   }
-
 }
